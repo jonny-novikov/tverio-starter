78ca9e3aac7612ddfb8929de6baf750271458055
Ivan Novikov <jonny.novikov@gmail.com>
2022-10-28 19:47:18 +0300

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  modules: [
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    'nuxt-icon'
+  ],
+  components: {
+    global: true,
+    dirs: [
+      '~/components',
+      '~/node_modules/@headlessui/vue/dist/components'
+    ]
+  },
+  experimental: {
+    reactivityTransform: true
+    // viteNode: true,
+  },
+  colorMode: {
+    classSuffix: ''
+  }
+})
